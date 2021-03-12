c6bb77de1f2a2ccfb1983dd0dae23d197e8a15e7
Dynotum <sedano.carlos93@gmail.com>
2021-03-11 20:10:35 -0600

    change to JUnit 5

--- src/test/java/com/csedano/pokeweb/controller/PokemonControllerTest.java
+++ src/test/java/com/csedano/pokeweb/controller/PokemonControllerTest.java
@@ -1,21 +1,19 @@
 package com.csedano.pokeweb.controller;
 
 import com.csedano.pokeweb.entity.Pokemon;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.awt.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.Assert.assertEquals;
-
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class PokemonControllerTest {
 
@@ -34,7 +32,7 @@ public class PokemonControllerTest {
         final ResponseEntity<Pokemon> entity = template.getForEntity("/rest", Pokemon.class);
         assertEquals(HttpStatus.OK, entity.getStatusCode());
         assertEquals(MediaType.APPLICATION_JSON, entity.getHeaders().getContentType());
-        final Pokemon pokemon= entity.getBody();
+        final Pokemon pokemon = entity.getBody();
         assertEquals("chikorita", pokemon.getName());
         assertEquals(152, pokemon.getId());
     }
---
+++ src/test/java/com/csedano/pokeweb/service/PokemonServiceTest.java
@@ -0,0 +1,35 @@
+package com.csedano.pokeweb.service;
+
+import com.csedano.pokeweb.entity.Pokemon;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class PokemonServiceTest {
+    private Logger logger = LoggerFactory.getLogger(PokemonServiceTest.class);
+    @Autowired
+    PokemonService pokemonService;
+
+    @Test
+    public void getPoke() {
+        Optional<Pokemon> optionPokemon = pokemonService.getById(1);
+
+        final Pokemon pokemon;
+        if (optionPokemon.isPresent()) {
+            pokemon = optionPokemon.get();
+            assertEquals(pokemon.getId(), 1);
+            assertEquals(pokemon.getName(), "bulbasaur");
+        }
+    }
+
+}
